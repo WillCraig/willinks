571256ee04194efa1cfcf2884e8e87ad29dec8a6
will craig <wdcraig99@gmail.com>
2026-04-20 11:28:49 -0400

    change Guid object for string in new sqlite database.

--- src/Willinks.Api/Models/Link.cs
+++ src/Willinks.Api/Models/Link.cs
@@ -2,7 +2,7 @@ namespace Willinks.Api.Models;
 
 public class Link
 {
-    public Guid Id { get; set; }
+    public string Id { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
     public string Destination { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
